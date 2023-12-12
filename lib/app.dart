64284245229fbec691029c85b5465680ea2e6501
dongeranguk <rooetym@naver.com>
2023-12-12 23:20:09 +0900

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WiseApp extends StatefulWidget {
+  const WiseApp({super.key});
+
+  @override
+  State<WiseApp> createState() => _WiseAppState();
+}
+
+class _WiseAppState extends State<WiseApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:playground/app.dart';
+
+void main() {
+  runApp(WiseApp());
+}
