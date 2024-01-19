07fc8f86738670b334c46d955b9c7f31d8dbf5a4
dongeranguk <rooetym@naver.com>
2024-01-19 23:54:30 +0900

    #9 - 캘린더 화면 만들기 2
    드롭다운 버튼의 선택 값에 따라 보여질 캘린더 화면을 구성해보았다.

--- lib/screen/main/tab/calendar/f_calendar.dart
+++ lib/screen/main/tab/calendar/f_calendar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:playground/screen/main/tab/calendar/vo/vo_schedule_list.dart';
-import 'package:playground/screen/main/tab/calendar/vo/vo_schedule_list.dart';
 import 'package:playground/screen/main/tab/calendar/w_schedule_list.dart';
 
 class CalendarFragment extends StatefulWidget {
@@ -13,6 +12,8 @@ class CalendarFragment extends StatefulWidget {
 }
 
 class _CalendarFragmentState extends State<CalendarFragment> {
+  final CalendarType _type = CalendarType.every;
+
   @override
   void initState() {
     super.initState();
@@ -20,53 +21,130 @@ class _CalendarFragmentState extends State<CalendarFragment> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
+    return _type == CalendarType.every
+        ? Scaffold(
+        appBar: AppBar(
+          title: Column(
+            children: [
+              CalendarTypeMenu(
+                type: _type,
+                callback: selectType,
+              ),
+            ],
+          ),
+        ),
         body: SingleChildScrollView(
           scrollDirection: Axis.vertical,
           child: Column(
-            children: scheduleList.map((e) => ScheduleList(userSchedule: e)).toList()
+            children: [
+              Column(
+                  children: scheduleList
+                      .map((e) => ScheduleList(userSchedule: e))
+                      .toList()),
+            ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             showModalBottomSheet(
-                context: context,
-                builder: (context) => SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.9,
-                      child: Column(
-                        children: [
-                          Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                TextButton(
-                                  child: const Text('취소',
-                                      style: TextStyle(fontSize: 17)),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
+              context: context,
+              builder: (context) =>
+                  SizedBox(
+                    height: MediaQuery
+                        .of(context)
+                        .size
+                        .height * 0.9,
+                    child: Column(
+                      children: [
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextButton(
+                                child: const Text('취소',
+                                    style: TextStyle(fontSize: 17)),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              const Text('새로운 이벤트',
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w600)),
+                              TextButton(
+                                child: const Text(
+                                  '추가',
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.w600),
                                 ),
-                                const Text('새로운 이벤트',
-                                    style: TextStyle(
-                                        fontSize: 17,
-                                        fontWeight: FontWeight.w600)),
-                                TextButton(
-                                  child: const Text(
-                                    '추가',
-                                    style: TextStyle(
-                                        fontSize: 17,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                  onPressed: () {},
-                                )
-                              ])
-                        ],
-                      ),
+                                onPressed: () {},
+                              )
+                            ])
+                      ],
                     ),
-                isScrollControlled: true,
+                  ),
+              isScrollControlled: true,
             );
           },
           child: const Icon(Icons.add),
-        ));
+        ))
+        : SizedBox();
+  }
+
+  void selectType() {
+    setState(() {});
   }
 }
+
+enum CalendarType {
+  personal('개인'),
+  every('모두');
+
+  final String type;
+  final Widget? calendarType;
+
+  const CalendarType(this.type, {this.calendarType});
+}
+
+
+//
+// class CalendarTypeMenu extends StatefulWidget {
+//   final CalendarType type;
+//
+//   const CalendarTypeMenu({required this.type, super.key});
+//
+//   @override
+//   State<CalendarTypeMenu> createState() => _CalendarTypeMenuState();
+// }
+//
+// class _CalendarTypeMenuState extends State<CalendarTypeMenu> {
+//   late CalendarType _selectedMenu;
+//
+//   @override
+//   void initState() {
+//     _selectedMenu = widget.type;
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return DropdownMenu(
+//       label: Text(_selectedMenu.type),
+//       menuStyle: MenuStyle(),
+//       dropdownMenuEntries: <DropdownMenuEntry<CalendarType>>[
+//         DropdownMenuEntry(
+//             value: CalendarType.every, label: CalendarType.every.type),
+//         DropdownMenuEntry(
+//             value: CalendarType.personal, label: CalendarType.personal.type),
+//         // DropdownMenuEntry(value: SampleItem.itemOne, label: '개인'),
+//       ],
+//       onSelected: (selectedMenu) {
+//         if (selectedMenu != null) {
+//           setState(() {
+//             _selectedMenu = selectedMenu;
+//           });
+//         }
+//       },
+//     );
+//   }
+// }
